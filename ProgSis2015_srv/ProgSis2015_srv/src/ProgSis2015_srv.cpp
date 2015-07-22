e1a0411ffff175e4508cf79478512cdf311056ec
davide <davide@dhcppc0>
2015-07-22 19:52:09 +0200

    Prime variabili

--- ProgSis2015_srv/ProgSis2015_srv/src/ProgSis2015_srv.cpp
+++ ProgSis2015_srv/ProgSis2015_srv/src/ProgSis2015_srv.cpp
@@ -2,14 +2,34 @@
 // Name        : ProgSis2015_srv.cpp
 // Author      : Davide
 // Version     :
-// Copyright   : 
-// Description : Hello World in C++, Ansi-style
+// Copyright   :
+// Description : Main program
 //============================================================================
 
 #include <iostream>
+#include <cstdlib>
+#include <sys/socket.h>
+
+
+#define COMM_LEN 35
+#define FILE_NAME_LEN 30
+
 using namespace std;
 
+void terminate_handler() {
+	cerr<<"Chiusura forzata processo\n";
+	exit (-1);
+}
+
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	int port, result;
+	char comm[COMM_LEN], nmfile[FILE_NAME_LEN];
+	struct sockaddr_in saddr, claddr = { 0 };
+	socklen_t claddr_len = sizeof(struct sockaddr_in);
+	int s, s_c; //Socket
+
+
+
+
 	return 0;
 }
